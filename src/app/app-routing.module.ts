61bfc63ca007f3aca9935dd3bc59a437a38a8cae
Devin Kelly-Collins <dkellycollins@gmail.com>
2020-10-12 12:56:06 +0000

    Updated links to use https.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,22 +18,22 @@ const routes: Routes = [
   {
     path: 'slides/javascript-not-javascript',
     component: FramePage,
-    data: { src: 'http://github.dkellycollins.info/managing-state-in-react/#/' }
+    data: { src: 'https://github.dkellycollins.info/managing-state-in-react/#/' }
   },
   {
     path: 'slides/managing-state-in-react',
     component: FramePage,
-    data: { src: 'http://github.dkellycollins.info/javascript-not-javascript/index.html#/' }
+    data: { src: 'https://github.dkellycollins.info/javascript-not-javascript/index.html#/' }
   },
   {
     path: 'slides/angular-cucumber-e2e',
     component: FramePage,
-    data: { src: 'http://github.dkellycollins.info/angular-cucumber-e2e/#/' }
+    data: { src: 'https://github.dkellycollins.info/angular-cucumber-e2e/#/' }
   },
   { 
     path: 'slides/developing-with-docker',
     component: FramePage,
-    data: { src: 'http://github.dkellycollins.info/talk-developing-with-docker/#/' }
+    data: { src: 'https://github.dkellycollins.info/talk-developing-with-docker/#/' }
   },
 
   { path: '**', redirectTo: 'about/resume' }
